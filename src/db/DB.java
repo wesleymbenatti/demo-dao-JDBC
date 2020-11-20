11c54f042c8d54c1142d7fc047f9d3f0f18b8f59
Wesley Moraes Benatti <wesleymbenatti@gmail.com>
2020-11-19 23:54:48 -0300

    Project created

---
+++ src/db/DB.java
@@ -0,0 +1,81 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+
+	// método statico que retorna Connection, props recebe loadProperties que leu o
+	// arquivo db.properties
+	// String url recebe a url do banco de dados e conn recebe a conexão do banco.
+
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	// método que retorna void, para fechar a conexão com o banco.
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+	}
+
+	// método static que retorna Properties, dentro do try, fs recebe a stream
+	// db.properties
+	// e a leitura é feita pelo objeto Properties, retornando o mesmo.
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (DbException | IOException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+
+}
