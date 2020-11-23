979182b4b75970105ed06b630342ec40ddf820e5
Wesley Moraes Benatti <wesleymbenatti@gmail.com>
2020-11-23 18:49:45 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,27 +14,41 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== test 1 : seller findById ===");
-		
+
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		System.out.println("\n=== test 2 : seller findByDepartment ===");
 		Department department = new Department(2, null);
+		//list recebendo a pesquisa por departamento no banco
 		List<Seller> list = sellerDao.findByDepartment(department);
+		//imprimindo todos os objetos encontrados no banco
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
 		System.out.println("\n=== test 3 : seller findAll ===");
+		//lista recebendo a pesquisa do todos os vendedores
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
 		System.out.println("\n=== test 4 : seller insert ===");
+		//criando novo seller
 		Seller newSeller = new Seller(null, "Wesley", "wesley@gmail.com", new Date(), 4000.00, department);
+		//inserindo novo seller no banco
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id: " + newSeller.getId());
+		
+		System.out.println("\n=== test 5 : seller update ===");
+		//seller recebe o resultado da pesquisa no banco
+		seller = sellerDao.findById(1);
+		//setando nome para seller
+		seller.setName("Martha Waine");
+		//atualizando seller
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 	
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -28,39 +28,34 @@ public class SellerDaoJDBC implements SellerDao {
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement("INSERT INTO seller "
-					+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+"VALUES "
-					+"(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDay().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
-			
+
 			// número de linhas alteradas
 			int rowsAffected = st.executeUpdate();
-			
-			if(rowsAffected > 0) {
+
+			if (rowsAffected > 0) {
 				// rs recebe a chave gerada(ID)
 				ResultSet rs = st.getGeneratedKeys();
-				if(rs.next()) {
-					//id recebe rs.getInt na posição 1.
+				if (rs.next()) {
+					// id recebe rs.getInt na posição 1.
 					int id = rs.getInt(1);
-					//atribuindo o id ao obj
+					// atribuindo o id ao obj
 					obj.setId(id);
 				}
 				DB.closeResultSet(rs);
-			}
-			else {
+			} else {
 				throw new DbException("Unexpected error! No rows affected.");
 			}
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 		}
 
@@ -68,6 +63,26 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
+
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDay().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
@@ -145,7 +160,6 @@ public class SellerDaoJDBC implements SellerDao {
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
 
-			
 			while (rs.next()) {
 				// tentando buscar um departamento dentro do map, se não existir, retorna null.
 				Department dep = map.get(rs.getInt("DepartmentId"));
